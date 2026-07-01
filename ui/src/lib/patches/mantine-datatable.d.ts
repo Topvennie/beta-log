fb875996eec022b108878f27d6284c4c857eae1c
vincent <vincent@vallaeys.com>
2026-07-01 20:12:46 +0200

    deps(ui): add mantine datatable

---
+++ ui/src/lib/patches/mantine-datatable.d.ts
@@ -0,0 +1,60 @@
+import { CollapseProps, MantineColor, MantineTheme } from "@mantine/core";
+import { ReactNode } from "react";
+
+// https://github.com/icflorescu/mantine-datatable/issues/651
+declare module "mantine-datatable" {
+  // https://icflorescu.github.io/mantine-datatable/type-definitions/
+  // Expand the types as needed
+
+  interface Column<T> extends Record<string, unknown> {
+    accessor: keyof T | string;
+    title?: string;
+    width?: number | string;
+    render?: (row: T) => ReactNode;
+    sortable?: boolean;
+    textAlign?: "left" | "right";
+  }
+
+  export interface DataTableProps<T> {
+    idAccessor?: keyof T;
+    columns: Column<T>[];
+    records: T[];
+    rowExpansion?: {
+      content: (params: { record: T, index: number, collapse: () => void }) => ReactNode,
+      collapseProps?: CollapseProps,
+      allowMultiple?: boolean,
+      expandable?: (params: { record: T, index: number }) => boolean,
+      trigger?: "click" | "always" | "never",
+      expanded?: {
+        recordIds: unknown[],
+        onRecordIdsChange?: React.Dispatch<React.SetStateAction<any[]>> | ((recordIds: unknown[]) => void); // eslint-disable-line @typescript-eslint/no-explicit-any
+      },
+    };
+
+    noHeader?: boolean;
+    noRecordsText?: string;
+    noRecordsIcon?: ReactNode;
+    backgroundColor?: string;
+    minHeight?: number;
+    maxHeight?: number;
+    height?: number;
+    borderRadius?: "xs" | "sm" | "md" | "lg" | "xl";
+    withTableBorder?: boolean;
+    rowBorderColor?: string;
+    rowBackgroundColor?: (record: T, index: number) => MantineColor | undefined | { light: MantineColor; dark: MantineColor };
+    highlightOnHover?: boolean;
+
+    fetching?: boolean;
+    customLoader?: ReactNode;
+    onScrollToBottom?: () => void;
+
+    selectionTrigger?: "cell" | "checkbox";
+
+    styles?: {
+      root: (theme: MantineTheme) => Record<string, string>,
+      header: (theme: MantineTheme) => Record<string, string>,
+    };
+  }
+
+  export function DataTable<T>(props: DataTableProps<T>): React.ReactNode;
+}
